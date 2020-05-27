2e20b4fedbdc38c30b2b73b3bcacae379b607014
Vladimir Bezobrazov <vovan0503@mail.ru>
2020-05-27 21:30:20 +0300

    Updates

---
+++ precache-manifest.646e0e320f4ea300aaf450edb5dce969.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5d8546363aac6bd67ad2d664eba03e",
+    "url": "/bike-accounting-client/index.html"
+  },
+  {
+    "revision": "33ab9615ecbf557fa4e2",
+    "url": "/bike-accounting-client/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "8236bbae57a2142024cd",
+    "url": "/bike-accounting-client/static/js/2.d43fc310.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bike-accounting-client/static/js/2.d43fc310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ab9615ecbf557fa4e2",
+    "url": "/bike-accounting-client/static/js/main.481d2617.chunk.js"
+  },
+  {
+    "revision": "adf39b05b9482fad4bb4",
+    "url": "/bike-accounting-client/static/js/runtime-main.6e4e8fc9.js"
+  },
+  {
+    "revision": "158be71842e4086e638cc8a073ee4a15",
+    "url": "/bike-accounting-client/static/media/RegImg.158be718.jpg"
+  },
+  {
+    "revision": "86f96adce82cb94488983fd2b59196b1",
+    "url": "/bike-accounting-client/static/media/employess.86f96adc.jpg"
+  },
+  {
+    "revision": "5c8c27cd4e27b8edc3240ba08ffcaf29",
+    "url": "/bike-accounting-client/static/media/home.5c8c27cd.jpg"
+  },
+  {
+    "revision": "560fe7862ae7c35daba7dd832c8f7abf",
+    "url": "/bike-accounting-client/static/media/stolen.560fe786.jpg"
+  },
+  {
+    "revision": "036b18c179225303ce03c123e272b721",
+    "url": "/bike-accounting-client/static/media/theftImg.036b18c1.jpg"
+  }
+]);
